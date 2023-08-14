3d806766c4ff6b0dd3acba4f7dd58077e72e476f
Anthony Ruiz <yobakaj@gmail.com>
2023-08-14 15:44:24 -0400

    fixed all items

--- db/client.js
+++ db/client.js
@@ -1,10 +1,22 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/gamestore';
+// const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/gamestore';
+const user = "postgres";
+const host = "localhost";
+const database = "gamestore";
+const port = "5432";
+const password = "Aa19525295$";
 
 const client = new Client({
-  connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  host,
+  port,
+  database,
+  user,
+  password,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = client;
